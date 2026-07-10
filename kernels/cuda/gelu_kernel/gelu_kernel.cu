590f44fd1e6248dc235d2e93b750679201f6c431
akshay919191 <sharmaakshay86117@gmail.com>
2026-07-10 10:14:56 +0530

    gelu backward added

--- kernels/cuda/gelu_kernel/gelu_kernel.cu
+++ kernels/cuda/gelu_kernel/gelu_kernel.cu
@@ -83,9 +83,68 @@ __global__ void gelufwd_kernel(
 template<int Br>
 __global__ void gelubwd_kernel(
     const __half* __restrict__ input,
-         __half* __restrict__ output ,
+    const __half* __restrict__ dl_dy,
+          __half* __restrict__ output ,
     const int seqlen , const int headdim , const int numhead
 )
 {
     int tid = threadIdx.x;
+
+    const int batchid = blockIdx.x;
+    const int headid  = blockIdx.y;
+    const int tileid  = blockIdx.z;
+
+    const long long base    = (long long)batchid * numhead * seqlen * headdim +
+                        (long long)headid  * seqlen * headdim;
+
+    const __half* INptr  = input  + base;
+    const __half* prevv  = dl_dy  + base;
+    
+          __half* outptr = output + base;
+
+        
+    extern __shared__ char smem[];
+    
+    char* ptr = smem;
+
+    ptr = reinterpret_cast<char*>(
+        (reinterpret_cast<uintptr_t>(ptr) + 31) & ~31ULL
+    );
+
+    __half* smemA = reinterpret_cast<__half*>(ptr);
+    ptr += Br * (headdim + PADDING) * sizeof(__half);
+
+    ptr = reinterpret_cast<char*>(
+        (reinterpret_cast<uintptr_t>(ptr) + 31) & ~31ULL
+    );
+
+    __half* upstream = reinterpret_cast<__half*>(ptr);
+    ptr += Br * (headdim + PADDING) * sizeof(__half);
+
+    cpasynccopygelu<Br>(prevv , upstream , headdim + PADDING , tileid , seqlen , headdim);
+    asm volatile("cp.async.commit_group;\n");
+
+    cpasynccopygelu<Br>(INptr , smemA , headdim + PADDING , tileid , seqlen , headdim);
+    asm volatile("cp.async.commit_group;\n");
+
+    asm volatile("cp.async.wait_group 0;\n");
+
+    __syncthreads();
+
+    performGelubck<Br>(smemA , upstream , headdim + PADDING , headdim);
+
+    for (int i = headdim * Br + tid; i < Br * headdim; i += blockDim.x)
+    {
+        int row = i / headdim;
+        int col = i % headdim;
+
+        if (col < headdim)
+            continue;   
+        int globalRow = tileid * Br + row;
+        if (globalRow >= seqlen)
+            continue;
+
+        outptr[globalRow * headdim + col] =
+            smemA[row * rowStride + col];
+    }
 }
