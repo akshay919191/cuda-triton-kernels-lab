e321c16d9ccdd48fc169225a2bb93fd2c10371a8
akshay919191 <sharmaakshay86117@gmail.com>
2026-07-10 20:16:30 +0530

    warp max reduction fixed

--- kernels/cuda/softmax_kernel/private_helper.cuh
+++ kernels/cuda/softmax_kernel/private_helper.cuh
@@ -2,7 +2,24 @@
 
 #include "../common/common_helper.cuh"
 
-// Private helpers for softmax kernel only.
-// Keep operator-specific helper functions here.
 
-// TODO: add softmax-specific helper functions.
+#include <torch/extension.h>
+#include <cuda_runtime.h>
+#include <cuda_fp16.h>
+#include <cuda.h>
+#include <math.h>
+#include <stdint.h>
+#include <float.h>
+
+template<int Br>
+__device__ __shared__ void dosoftmax(
+    const __half* __restrict__ data,
+          float*  __restrict__ rsult,
+    int headdim , int seqlen , int rowstride
+)
+{
+    int tid = threadIdx.x;
+
+    //// we will store all max in rsult , and then overwrite it with actual each row sum using this multiWarpReductionMax_half2
+    
+}
--- kernels/cuda/softmax_kernel/softmax_kernel.cu
+++ kernels/cuda/softmax_kernel/softmax_kernel.cu
@@ -1,4 +1,53 @@
 #include "../common/common_helper.cuh"
 #include "private_helper.cuh"
 
-// TODO: implement softmax CUDA kernel here.
+#include <cuda.h>
+#include <cuda_fp16.h>
+#include <cuda_runtime.h>
+#include <stdint.h>
+#include <math.h>
+#include <float.h>
+#include <iostream>
+#include <cmath>
+#define PADDING 8
+
+template<int Br>
+__global__ void softmaxfwd_kernel(
+    const __half* __restrict__ input,
+          __half* __restrict__ output,
+    const int headdim , const int seqlen , const int numhead
+)
+{
+    int tid = threadIdx.x;
+    const int rowstride = PADDING + headdim;
+
+    const int batchid = blockIdx.x;
+    const int headid  = blockIdx.y;
+    const int tileid  = blockIdx.z;
+
+    const long long base = (long long)batchid * numhead * headdim * seqlen + 
+                           (long long)headid  * headdim * seqlen;
+
+    const __half* INptr  = input + base;
+          __half* outptr = output + base;
+    
+    extern __shared__ char smem[];
+
+    char* ptr = smem;
+
+    ptr = reinterpret_cast<char*>(
+        (reinterpret_cast<uintptr_t>(ptr) + 31) & ~31ULL
+    );
+
+    __half* smemA = reinterpret_cast<__half*>(ptr);
+    ptr += Br * rowstride * sizeof(__half);
+
+    ptr = reinterpret_cast<char*>(
+        (reinterpret_cast<uintptr_t>(ptr) + 31) & ~31ULL
+    );
+
+    __half* res = reinterpret_cast<float*>(ptr);
+    ptr += Br * sizeof(float);
+
+    
+}
