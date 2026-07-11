506c631e486c8b9358d0e9bc74f89fd2f258cf03
akshay919191 <sharmaakshay86117@gmail.com>
2026-07-11 10:57:08 +0530

    lyer norm fwd

--- kernels/cuda/layernorm_kernel/layernorm_kernel.cu
+++ kernels/cuda/layernorm_kernel/layernorm_kernel.cu
@@ -1,3 +1,104 @@
 #include "../common/common_helper.cuh"
 #include "private_helper.cuh"
 
+#include <cuda.h>
+#include <cuda_fp16.h>
+#include <cuda_runtime.h>
+#include <stdint.h>
+#include <math.h>
+#include <float.h>
+#include <iostream>
+#include <cmath>
+#define PADDING 8
+
+template<int Br>
+__global__ void layernormfwd_kernel(
+    const __half* __restrict__ input,
+    const float*  __restrict__ gamma,
+    const float*  __restrict__ betaa,
+          __half* __restrict__ output,
+    float eps , const int headdim , int seqlen , int numhead
+){
+    int tid = threadIdx.x;
+
+    const int rowstride = headdim + PADDING;
+    const int batchid   = blockIdx.x;
+    const int headid    = blockIdx.y;
+    const int tileid    = blockIdx.z;
+
+    long long base = (long long)batchid * numhead * headdim * seqlen + 
+                     (long long)headid  * headdim * seqlen;
+    
+    const __half* INptr = input + base;
+          __half* outptr= output + base;
+
+    extern __shared__ char smem[];
+    char* ptr = smem;
+
+    ptr = reinterpret_cast<char*>(
+        (reinterpret_cast<uintptr_t>(ptr) + 31) & ~31ULL
+    );
+
+    __half* smemA = reinterpret_cast<__half*>(ptr);
+    ptr += Br * rowstride * sizeof(__half);
+
+    ptr = reinterpret_cast<char*>(
+        (reinterpret_cast<uintptr_t>(ptr) + 31) & ~31ULL
+    );
+
+    float* gam = reinterpret_cast<float*>(ptr);
+    ptr += headdim * sizeof(float);
+
+    ptr = reinterpret_cast<char*>(
+        (reinterpret_cast<uintptr_t>(ptr) + 31) & ~31ULL
+    );
+
+    float* beta = reinterpret_cast<float*>(ptr);
+    ptr += headdim * sizeof(float);
+
+    ptr = reinterpret_cast<char*>(
+        (reinterpret_cast<uintptr_t>(ptr) + 31) & ~31ULL
+    );
+
+    float* res1 = reinterpret_cast<float*>(ptr);
+    ptr += Br * sizeof(float);
+
+    float* res2 = reinterpret_cast<float*>(ptr);
+    ptr += Br * sizeof(float);
+
+    cpasynccopy<Br>(INptr , smemA , rowstride , tileid , seqlen , headdim);
+    asm volatile("cp.async.commit_group;\n");
+    asm volatile("cp.async.wait_group 0;\n");
+
+    __syncthreads();
+
+    for(int i = tid ; i < headdim ; i += blockDim.x)
+    {
+        gam[i] = gamma[i]; beta[i] = betaa[i];
+    }
+    __syncthreads();
+
+    multiWarpReductionMEAN(smemA , res1 , headdim , rowstride , Br);
+    __syncthreads();
+
+    multiWarpReductionSIGMA2(smemA , res1 , res2 , headdim , rowstride , Br);
+    __syncthreads();
+
+    //// now we have sigma square and U and x , gamma and beta
+    for(int i = tid ; i < Br * headdim ; i += blockDim.x)
+    {
+        int r = i / headdim;
+        int c = i % headdim;
+
+        int global_row = (tileid * Br + r);
+        if (global_row >= seqlen) continue;
+
+        int smemidx = r * rowstride + c;
+        float val = gam[r] * (__half2float(smemA[smemidx]) - res1[r]) / (sqrtf(res2[r] + eps)) + beta[r];
+        outptr[global_row * headdim + c] = val;
+
+    }
+}
+
+
+
--- kernels/cuda/layernorm_kernel/private_helper.cuh
+++ kernels/cuda/layernorm_kernel/private_helper.cuh
@@ -2,7 +2,89 @@
 
 #include "../common/common_helper.cuh"
 
-// Private helpers for layernorm kernel only.
-// Keep operator-specific helper functions here.
+#include <cuda_runtime.h>
+#include <stdio.h>
+#include <cuda_fp16.h>
+#include <stdint.h>
+#include <cuda.h>
+#include <cuda_fp16.h>
+#include <stdint.h>
+#include <math.h>
+#include <float.h>
 
-// TODO: add layernorm-specific helper functions.
+
+__device__ __forceinline__
+void multiWarpReductionMEAN(
+    const __half* __restrict__ input,
+    float* __restrict__ out,
+    int cols,
+    int rowStride,
+    int Br
+)
+{
+    int tid      = threadIdx.x;
+    int lane     = tid & 31;
+    int warpid   = tid >> 5;
+    int numWarps = blockDim.x >> 5;
+
+    for (int row = warpid; row < Br; row += numWarps)
+    {
+        const __half* rowPtr = input + row * rowStride;
+
+        float localsum = 0.0f;
+
+        const half2* rowPtr2 = reinterpret_cast<const half2*>(rowPtr);
+        int cols2 = cols >> 1;
+
+        for (int i = lane; i < cols2; i += WARP_SIZE)  /// warp size from common helper
+        {
+            half2 h = rowPtr2[i];
+            float2 f = __half22float2(h);
+
+            localsum += f.x;
+            localsum += f.y;
+        }
+
+        localsum = reducesum(localsum);
+
+        if (lane == 0)
+            out[row] = localsum / cols;
+    }
+}
+
+__device__ __forceinline__
+void multiWarpReductionSIGMA2(
+    const __half* __restrict__ input,
+    float* __restrict__ res,
+    float* __restrict__ out,
+    int cols,
+    int rowStride,
+    int Br
+){
+    int tid      = threadIdx.x;
+    int lane     = tid & 31;
+    int warpid   = tid >> 5;
+    int numWarps = blockDim.x >> 5;
+
+    for(int row = warpid ; row < Br ; row += numWarps)
+    {
+        const __half* rowptr = input + row * rowStride;
+
+        float localsigma = 0.f;
+
+        const half2* rowptr2 = reinterpret_cast<half2*>(rowptr);
+        int col2 = cols >> 1;
+
+        for(int i = lane ; i < col2 ; i += WARP_SIZE)
+        {
+            half2 h = rowptr2[i];
+            float2 f = __half22float2(h);
+
+            localsigma += (f.x - res[row]) * (f.x - res[row]);
+            localsigma += (f.y - res[row]) * (f.y - res[row]);
+        }
+        localsigma = reducesum(localsigma);
+        if(lane == 0) out[row] = localsigma / cols;
+    }
+
+}
